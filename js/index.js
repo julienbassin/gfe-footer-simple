c81ab700c3ee70b1704daa99c40712de65216da2
julien.bassin <julien.bassin@avanade.com>
2025-02-09 11:48:43 +0100

    feat: add date using JS script

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 // Write custom JavaScript here.
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
+
+let element = document.getElementById("copyrightYear");
+const date = new Date().getFullYear();
+
+element.textContent = date;
